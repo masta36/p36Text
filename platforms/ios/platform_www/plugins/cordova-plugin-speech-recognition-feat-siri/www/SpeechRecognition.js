bb190315b38bc2f95f6afe082f2ee288481e7075
Patrick Pfau <patrick.pfau@projektraum36.de>
2017-01-24 16:03:36 +0100

    added IOS platform

---
+++ platforms/ios/platform_www/plugins/cordova-plugin-speech-recognition-feat-siri/www/SpeechRecognition.js
@@ -0,0 +1,20 @@
+cordova.define("cordova-plugin-speech-recognition-feat-siri.SpeechRecognition", function(require, exports, module) {
+/*
+
+    Cordova Plugin Speech Recognition feat. Siri API
+    https://github.com/bluedesign/speech-recognition-feat-siri
+
+    by SOHKAKUDO Ltd.
+    MIT License
+
+*/
+
+var exec = require('cordova/exec');
+
+var SpeechRecognition = {
+  recordButtonTapped: function(limitationSec, onSuccess, onFail) {
+    exec(onSuccess, onFail, 'SpeechRecognition', 'recordButtonTapped', [limitationSec]);
+  }
+};
+module.exports = SpeechRecognition
+});
